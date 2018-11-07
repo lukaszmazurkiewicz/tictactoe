1e4f34cd3cfbd1e9a20c2516771b1efebad17e03
Maciej Rudnicki <maciejrudnicki11@gmail.com>
2018-11-07 19:45:17 +0100

    feat : player choose and computer choose

--- src/main/java/ticktacktoe/TickTackToeRunner.java
+++ src/main/java/ticktacktoe/TickTackToeRunner.java
@@ -22,14 +22,19 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 
 public class TickTackToeRunner extends Application {
 
+    private static final Random RANDOM = new Random();
     private static final double PAWN_SIZE_MULTIPLIER = 0.30;
     private static final Image IMAGE_FOR_BACKGROUND = new Image("Graphics/background.jpg");
-    private static final Image ANIMATION_FOR_X = new Image("Graphics/Draw_X_Anim.gif");
+    private static final Image ANIMATION_FOR_X = new Image("Graphics/cross.png");
+    private static final Image ANIMATION_FOR_Y = new Image("Graphics/circle.png");
     private static final Image IMAGE_FOR_CURSOR = new Image("Graphics/cursorIcon.png");
     private static final Image IMAGE_FOR_EMPTY_FIELD = new Image("Graphics/transparent.png");
 
@@ -45,6 +50,8 @@ public class TickTackToeRunner extends Application {
     private ImageView pawn31;
     private ImageView pawn32;
     private ImageView pawn33;
+
+    private Map<String, ImageView> pawns = new HashMap<>();
 
     private HBox topScoreBoard;
     private Text playerOneName, playerOneScore, playerTwoName, playerTwoScore;
@@ -97,32 +104,41 @@ public class TickTackToeRunner extends Application {
         gameBoardPane.setCursor(new ImageCursor(IMAGE_FOR_CURSOR));
         gameBoardPane.setAlignment(Pos.CENTER);
 
-        pawn11 = newPawn(gameBoardPane);
+        pawn11 = newPawn(gameBoardPane, 1, 1);
         gameBoardPane.add(pawn11, 1, 1);
+        pawns.put("11", pawn11);
 
-        pawn21 = newPawn(gameBoardPane);
+        pawn21 = newPawn(gameBoardPane, 2, 1);
         gameBoardPane.add(pawn21, 2, 1);
+        pawns.put("21", pawn21);
 
-        pawn31 = newPawn(gameBoardPane);
+        pawn31 = newPawn(gameBoardPane, 3, 1);
         gameBoardPane.add(pawn31, 3, 1);
+        pawns.put("31", pawn31);
 
-        pawn12 = newPawn(gameBoardPane);
+        pawn12 = newPawn(gameBoardPane, 1, 2);
         gameBoardPane.add(pawn12, 1, 2);
+        pawns.put("12", pawn12);
 
-        pawn22 = newPawn(gameBoardPane);
+        pawn22 = newPawn(gameBoardPane, 2, 2);
         gameBoardPane.add(pawn22, 2, 2);
+        pawns.put("22", pawn22);
 
-        pawn32 = newPawn(gameBoardPane);
+        pawn32 = newPawn(gameBoardPane, 3, 2);
         gameBoardPane.add(pawn32, 3, 2);
+        pawns.put("32", pawn32);
 
-        pawn13 = newPawn(gameBoardPane);
+        pawn13 = newPawn(gameBoardPane, 1, 3);
         gameBoardPane.add(pawn13, 1, 3);
+        pawns.put("13", pawn13);
 
-        pawn23 = newPawn(gameBoardPane);
+        pawn23 = newPawn(gameBoardPane, 2, 3);
         gameBoardPane.add(pawn23, 2, 3);
+        pawns.put("23", pawn23);
 
-        pawn33 = newPawn(gameBoardPane);
+        pawn33 = newPawn(gameBoardPane, 3, 3);
         gameBoardPane.add(pawn33, 3, 3);
+        pawns.put("33", pawn33);
 
 
         BorderPane borderPane = new BorderPane();
@@ -135,23 +151,54 @@ public class TickTackToeRunner extends Application {
         Scene scene = new Scene(borderPane, 600, 600, Color.BLACK);
 
         primaryStage.setTitle("Tick Tack Toe");
-        primaryStage.initStyle(StageStyle.UNIFIED);
         primaryStage.setScene(scene);
         primaryStage.show();
 
     }
 
-    private ImageView newPawn(GridPane gameBoardPane) {
+    private ImageView newPawn(GridPane gameBoardPane, int col, int row) {
         ImageView imageView = new ImageView(IMAGE_FOR_EMPTY_FIELD);
         imageView.fitWidthProperty().bind(gameBoardPane.widthProperty().multiply(PAWN_SIZE_MULTIPLIER));
         imageView.fitHeightProperty().bind(gameBoardPane.widthProperty().multiply(PAWN_SIZE_MULTIPLIER));
-        imageView.setOnMouseEntered(e -> handleMouseEntersCell(e));
-        imageView.setOnMouseExited(e -> handleMouseExitsCell(e));
+//        imageView.setOnMouseEntered(e -> handleMouseEntersCell(e));
+//        imageView.setOnMouseExited(e -> handleMouseExitsCell(e));
+        imageView.setOnMouseClicked(e -> handleUserClick(e, col, row));
 
         return imageView;
     }
 
+    private void handleUserClick(MouseEvent e, int col, int row) {
 
+        ImageView view = (ImageView) e.getSource();
+        view.setImage(ANIMATION_FOR_X);
+        System.out.println("Player Col " + col + " Row " + row);
+
+        verifyIfFinish();
+
+        handleComputerClick();
+    }
+
+    private void verifyIfFinish() {
+
+
+    }
+
+
+    private void handleComputerClick() {
+        int col = RANDOM.nextInt(3) + 1;
+        int row = RANDOM.nextInt(3) + 1;
+        System.out.println("Computer Col " + col + " Row " + row);
+
+        String key = String.valueOf(col) + String.valueOf(row);
+        ImageView imageView = pawns.get(key);
+
+        if (imageView.getImage() != ANIMATION_FOR_Y && imageView.getImage() != ANIMATION_FOR_X) {
+            imageView.setImage(ANIMATION_FOR_Y);
+        } else {
+            System.out.println("Field is taked");
+            handleComputerClick();
+        }
+    }
 
     private void handleButtonClick(MouseEvent event) {
 
